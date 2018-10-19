38346bcd4d7f15f3aa3da0f97831c1596257c456
HighRiseCode <highrisecode@gmail.com>
2018-10-19 08:40:29 -0500

    Add react to ss

---
+++ single-stage/web/src/component/box.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Box extends React.Component{
+  render(){
+    return(
+      <div className="wrapper">
+        <div
+          style={{backgroundColor:`${this.props.color}`}}
+          className="box">
+        </div>
+        <button onClick={()=>{this.props.handleClick()}}>Change Color</button>
+      </div>
+    )
+  }
+}
+
+export default Box;
